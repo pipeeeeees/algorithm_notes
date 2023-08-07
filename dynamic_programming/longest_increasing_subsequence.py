07376384d70044e12f9fb2f3a4c594197d7967b0
pipeeeeees <48690614+pipeeeeees@users.noreply.github.com>
2023-08-07 17:05:55 -0400

    Update longest_increasing_subsequence.py

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -7,13 +7,15 @@ def lis_length(arr):
     if n == 0:
         return 0
 
-    # list initialized at 1
+    # list initialized at LIS = 1 for all
     dp = [1] * n
 
+    # run through every combination of increasing subsequences and store only the greater incs length
     for i in range(1, n):
         for j in range(i):
             if arr[i] > arr[j]:
                 dp[i] = max(dp[i], dp[j] + 1)
+    # return the greatest magnitude
     # runs in O(n^2) time
     return max(dp)
 
